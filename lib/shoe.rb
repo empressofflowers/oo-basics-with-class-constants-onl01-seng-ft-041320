87b708d58b36ac799736af54eddd564af99a8064
Devone Austin <devone.austin@gmail.com>
2020-04-22 16:51:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  brands = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -15,7 +15,7 @@ class Shoe
 
   def brands=(brand)
     @brand = brand
-    brands << brand
+    BRANDS << brand
   end
 
 end
