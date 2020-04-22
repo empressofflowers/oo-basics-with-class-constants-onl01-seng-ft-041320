27120f2ba18fe1935859b5f9e2a8ea3dfc6153ee
Devone Austin <devone.austin@gmail.com>
2020-04-22 16:42:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,9 +2,9 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  def initialize(title, genres)
+  def initialize(title, genre)
     @title = title
-    @genres = genres
+    @genres = genre
   end
 
   def turn_page
